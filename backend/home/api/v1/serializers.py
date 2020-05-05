070146b3e970123ac7ab97d9d298761d42defe81
Crowdbotics <team@crowdbotics.com>
2020-05-05 17:40:20 +0000

    Update models

--- backend/home/api/v1/serializers.py
+++ backend/home/api/v1/serializers.py
@@ -8,7 +8,7 @@ from allauth.account.adapter import get_adapter
 from allauth.account.utils import setup_user_email
 from rest_framework import serializers
 from rest_auth.serializers import PasswordResetSerializer
-from home.models import HomePage, CustomText
+from home.models import CustomText, HomePage
 
 User = get_user_model()
 
