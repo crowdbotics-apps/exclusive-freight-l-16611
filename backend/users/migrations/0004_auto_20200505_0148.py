4bc00be6df65e878836e66adda53f3edb6a1a0e6
Crowdbotics <team@crowdbotics.com>
2020-05-05 01:58:21 +0000

    Update models

--- backend/users/migrations/0004_auto_20200505_0148.py
+++ backend/users/migrations/0004_auto_20200505_0148.py
@@ -6,31 +6,25 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('users', '0003_auto_20200505_0146'),
+        ("users", "0003_auto_20200505_0146"),
     ]
 
     operations = [
-        migrations.RemoveField(
-            model_name='user',
-            name='last_updated',
-        ),
-        migrations.RemoveField(
-            model_name='user',
-            name='timestamp_created',
-        ),
+        migrations.RemoveField(model_name="user", name="last_updated",),
+        migrations.RemoveField(model_name="user", name="timestamp_created",),
         migrations.AlterField(
-            model_name='user',
-            name='email',
+            model_name="user",
+            name="email",
             field=models.EmailField(blank=True, max_length=254, null=True),
         ),
         migrations.AlterField(
-            model_name='user',
-            name='first_name',
+            model_name="user",
+            name="first_name",
             field=models.TextField(blank=True, null=True),
         ),
         migrations.AlterField(
-            model_name='user',
-            name='last_name',
+            model_name="user",
+            name="last_name",
             field=models.TextField(blank=True, null=True),
         ),
     ]
